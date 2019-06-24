5a0540cc134bbc868410bb5bf200f206acef1900
Vivek Kumar <axis.vivekk@gmail.com>
2019-06-24 17:19:49 +0530

    OkHTTP added

--- Project2/welcome/src/main/java/axisvapix/TestLogin.java
+++ Project2/welcome/src/main/java/axisvapix/TestLogin.java
@@ -1,23 +1,67 @@
 package axisvapix;
 
-import kong.unirest.Unirest;
-import kong.unirest.HttpResponse;;
+import com.mashape.unirest.http.HttpResponse;
+//import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.Request;
 
 public class TestLogin {
-    public static void TestHello() {
-            System.out.println("A new instance of the Test class was created!");
+    public static void TestHello() throws UnirestException, InterruptedException {
+            System.out.println("Inside Function:");
+            HttpResponse<String> response = Unirest.post("http://192.168.1.155/axis-cgi/param.cgi")
+            .header("accept", "application/json")
+            //action=list&responseformat=rfc
+            .queryString("action", "list")
+            .queryString("responseformat", "rfc")
+            .basicAuth("root", "pass")
+            //.field("parameter", "value")
+            //.field("foo", "bar")
+            .asString();
+            System.out.println("Body:\n"+response.getBody());
+            System.out.println("String:\n"+response.toString());
     }
+
+    public static void TestDigest() throws UnirestException, InterruptedException {
+        System.out.println("Inside Digest Function:");
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+        .url("http://192.168.1.155/axis-cgi/param.cgi?action=list&responseformat=rfc")
+        .get()
+        .addHeader("cache-control", "no-cache")
+        .addHeader("Postman-Token", "af3f16a3-167c-417f-aee4-518164f63494")
+        .build();
+        try
+        {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.toString());
+        }
+        catch(Exception e1)
+        {
+            System.out.println(e1.toString());
+        }
+      
+        
+}
+
+
 
     public static void main(String[] args) {
             System.out.println("Sending Request...");
-        HttpResponse<String> response = Unirest.post("http://192.168.1.155/#settings/image")
-        .header("accept", "application/json")
-        //.queryString("action", "list")
-        //.queryString("responseformat", "rfc")
-        .basicAuth("root", "pass")
-        //.field("parameter", "value")
-        //.field("foo", "bar")
-        .asString();
-        System.out.println(response.getBody());
+            try
+            {
+                //TestHello();
+                TestDigest();
+            }
+            catch(Exception e1)
+            {
+                System.out.println(e1.toString());
+            }
+            
     }
 }
